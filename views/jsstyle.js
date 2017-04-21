a933c67ca3bc14ae5af96738ca3e4fda458453ca
eldiablotrabajo <alexbiddle72@gmail.com>
2017-04-21 20:25:15 +0100

    Changing weeks for timeline code complete

--- views/jsstyle.js
+++ views/jsstyle.js
@@ -1,5 +1,58 @@
 const getStudentEmail = document.getElementById("registerEmail");
 const getStudentPassword = document.getElementById("registerPassword");
+const btnPreviousWeek = document.getElementById("btnPreviousWeek");
+
+function changeWeekPrevious(){
+    const workWeek = document.getElementById("workWeek"); //contains value of element with id of workWeek
+    if(workWeek.innerHTML==="Week 5"){
+        workWeek.innerHTML="Week 4";
+    }
+     //onclick set innerHTML of this element to week 4
+}
+
+function changeWeekNext(){
+    const workWeek = document.getElementById("workWeek"); //contains value of element with id of workWeek
+    workWeek.innerHTML="Week 6"; //onclick set innerHTML of this element to week 4
+}
+
+function hideWorksheetOne(){
+    let workSheetOne = document.getElementById("worksheetOne");
+    if(workSheetOne.style.opacity!==0){
+        workSheetOne.style.opacity=0;
+        workSheetOne.style.display='none'
+    }else{
+        workSheetOne.style.opacity=1;
+        workSheetOne.style.display='inline'
+    }
+}
+
+function hideWorksheetThree(){
+    let workSheetThree = document.getElementById("worksheetThree");
+    if(workSheetThree.style.opacity!==0){
+        workSheetThree.style.opacity=0;
+        workSheetThree.style.display='none'
+    }else{
+        workSheetThree.style.opacity=1;
+        workSheetThree.style.display='inline'
+    }
+}
+
+function hideWorksheetFour(){
+    let workSheetFour = document.getElementById("workSheetFour");
+    let workSheetTwo = document.getElementById("workSheetTwo");
+    let workSheetThree = document.getElementById("workSheetThree");
+    if(workSheetFour.style.opacity!==0){
+        workSheetFour.style.opacity=0;
+        workSheetFour.style.display='none'
+    }else{
+        workSheetTwo.style.opacity=1;
+        workSheetTwo.style.display='inline';
+        workSheetThree.style.opacity=1;
+        workSheetThree.style.display='inline';
+        workSheetFour.style.opacity=1;
+        workSheetFour.style.display='inline';
+    }
+}
 
 function checkPWLength(){
     if(getStudentPassword.length<6){
@@ -47,6 +100,13 @@ function showModule(moduleName) {
 //function to increase value of progress bar every time work is dropped into completed divs
 function increaseProgress(val){
     document.getElementById("workSheetProgress").value=val;
-    
+    const getProgressBarValue = document.getElementById("progressBarValue");
+    getProgressBarValue.textContent=val + "%";
+    if(val===100){
+        getProgressBarValue.textContent="Work completed for the work"
+    }
     //get progress bar element and set its value to whatever is entered into the parameter of the function
 }
+
+
+
